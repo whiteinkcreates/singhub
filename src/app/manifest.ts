50600ace9a5e8f12338410a3a7eefe829e4141d4
whiteinkcreates <whiteinkcreates@gmail.com>
2026-07-05 21:00:44 -0700

    Fix manifest icon purpose type

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -14,13 +14,18 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/icon.png",
         sizes: "512x512",
         type: "image/png",
-        purpose: "any maskable",
+        purpose: "maskable",
+      },
+      {
+        src: "/icon.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any",
       },
       {
         src: "/apple-icon.png",
         sizes: "180x180",
         type: "image/png",
-        purpose: "any",
       },
     ],
   };
