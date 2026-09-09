d241ab1e363f60bd1b36f49db8ed1dcd96533201
whiteinkcreates <whiteinkcreates@gmail.com>
2026-09-09 00:44:54 -0700

    Fix PWA icon purpose types

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -24,7 +24,13 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/icon.png",
         sizes: "512x512",
         type: "image/png",
-        purpose: "any maskable",
+        purpose: "any",
+      },
+      {
+        src: "/icon.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
       },
       {
         src: "/apple-icon.png",
