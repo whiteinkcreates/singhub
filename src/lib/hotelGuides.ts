ff2406d71f6c1fe5dbeeb055f2317cbfdf537ffe
whiteinkcreates <whiteinkcreates@gmail.com>
2026-09-25 13:19:35 -0700

    Wire verified hotel hero images and wordmarks

--- src/lib/hotelGuides.ts
+++ src/lib/hotelGuides.ts
@@ -17,8 +17,8 @@ export type HotelGuide = {
 };
 
 const downtown: HotelGuide[] = [
-  { slug: "pendry-san-diego", name: "Pendry San Diego", shortName: "Pendry", area: "downtown", address: "550 J St, San Diego, CA 92101", latitude: 32.7083, longitude: -117.1594, heroFallback: "downtown" },
-  { slug: "hard-rock-san-diego", name: "Hard Rock Hotel San Diego", shortName: "Hard Rock", area: "downtown", address: "207 5th Ave, San Diego, CA 92101", latitude: 32.7077, longitude: -117.1600, heroFallback: "downtown" },
+  { slug: "pendry-san-diego", name: "Pendry San Diego", shortName: "Pendry", area: "downtown", address: "550 J St, San Diego, CA 92101", latitude: 32.7083, longitude: -117.1594, heroImageUrl: "https://uploads.pendry.com/redesign/wp-content/uploads/sites/2/2018/12/10211411/1-4.jpeg", wordmarkImageUrl: "https://upload.wikimedia.org/wikipedia/commons/3/3f/Pendry_Hotels_Logo.png", heroFallback: "downtown" },
+  { slug: "hard-rock-san-diego", name: "Hard Rock Hotel San Diego", shortName: "Hard Rock", area: "downtown", address: "207 5th Ave, San Diego, CA 92101", latitude: 32.7077, longitude: -117.1600, heroImageUrl: "https://hotel.hardrock.com/san-diego/files/6076/HRSD_Exterior_2200x1467.jpg", heroFallback: "downtown" },
   { slug: "hilton-gaslamp", name: "Hilton San Diego Gaslamp Quarter", shortName: "Hilton Gaslamp", area: "downtown", address: "401 K St, San Diego, CA 92101", latitude: 32.7088, longitude: -117.1617, heroFallback: "downtown" },
   { slug: "ac-hotel-gaslamp", name: "AC Hotel San Diego Downtown Gaslamp Quarter", shortName: "AC Hotel Gaslamp", area: "downtown", address: "743 5th Ave, San Diego, CA 92101", latitude: 32.7130, longitude: -117.1599, heroFallback: "downtown" },
   { slug: "margaritaville-gaslamp", name: "Margaritaville Hotel San Diego Gaslamp Quarter", shortName: "Margaritaville", area: "downtown", address: "435 6th Ave, San Diego, CA 92101", latitude: 32.7101, longitude: -117.1581, heroFallback: "downtown" },
@@ -35,8 +35,8 @@ const downtown: HotelGuide[] = [
 ];
 
 const laJolla: HotelGuide[] = [
-  { slug: "la-valencia", name: "La Valencia Hotel", shortName: "La Valencia", area: "la-jolla", address: "1132 Prospect St, La Jolla, CA 92037", latitude: 32.8480, longitude: -117.2733, heroFallback: "coast", quickTripMiles: 5, standoutMiles: 9 },
-  { slug: "grande-colonial", name: "Grande Colonial", shortName: "Grande Colonial", area: "la-jolla", address: "910 Prospect St, La Jolla, CA 92037", latitude: 32.8464, longitude: -117.2747, heroFallback: "coast", quickTripMiles: 5, standoutMiles: 9 },
+  { slug: "la-valencia", name: "La Valencia Hotel", shortName: "La Valencia", area: "la-jolla", address: "1132 Prospect St, La Jolla, CA 92037", latitude: 32.8480, longitude: -117.2733, heroImageUrl: "https://assets.milestoneinternet.com/cdn-cgi/image/f%3Dauto/pacifica-host-hotels/la-valencia-hotel/site-images/meetings/pink-hotel-with-a-domed-tower.jpg?cropH=1500&cropW=4800&cropY=485&height=450&width=1440", wordmarkImageUrl: "https://la-valencia-hotel.myshopify.com/cdn/shop/files/LV_08-Hotel-and-Spa-MainLogo_Red.png?v=1768343477", heroFallback: "coast", quickTripMiles: 5, standoutMiles: 9 },
+  { slug: "grande-colonial", name: "Grande Colonial", shortName: "Grande Colonial", area: "la-jolla", address: "910 Prospect St, La Jolla, CA 92037", latitude: 32.8464, longitude: -117.2747, heroImageUrl: "https://thegrandecolonial.com/wp-content/uploads/2024/03/11202024_DroneExteriors_GrandeColonial_02.jpg", heroFallback: "coast", quickTripMiles: 5, standoutMiles: 9 },
   { slug: "pantai-inn", name: "Pantai Inn", shortName: "Pantai Inn", area: "la-jolla", address: "1003 Coast Blvd, La Jolla, CA 92037", latitude: 32.8471, longitude: -117.2762, heroFallback: "coast", quickTripMiles: 5, standoutMiles: 9 },
   { slug: "orli-la-jolla", name: "Orli La Jolla", shortName: "Orli", area: "la-jolla", address: "7753 Draper Ave, La Jolla, CA 92037", latitude: 32.8433, longitude: -117.2742, heroFallback: "coast", quickTripMiles: 5, standoutMiles: 9 },
   { slug: "cormorant-la-jolla", name: "Cormorant Boutique Hotel", shortName: "Cormorant", area: "la-jolla", address: "1110 Prospect St, La Jolla, CA 92037", latitude: 32.8475, longitude: -117.2740, heroFallback: "coast", quickTripMiles: 5, standoutMiles: 9 },
@@ -55,6 +55,8 @@ const laMesa: HotelGuide[] = [
     address: "8000 Parkway Drive, La Mesa, CA 91942",
     latitude: 32.77495,
     longitude: -117.0259,
+    heroImageUrl: "https://digital.ihg.com/is/image/ihg/holiday-inn-express-la-mesa-8924019411-4x3",
+    wordmarkImageUrl: "https://development.ihg.com/sites/ihgplc/files/IHG/americas/logo/holiday-inn-express-logo-img.png",
     heroFallback: "downtown",
     walkableMiles: 0.7,
     quickTripMiles: 3.5,
